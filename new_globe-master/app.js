61c449ab2272d6b948053be1de8e10f973a54e37
bhavanamamane <mamanebhava278@students.itu.edu>
2017-03-14 22:23:00 -0700

    ejs

--- new_globe-master/app.js
+++ new_globe-master/app.js
@@ -3,6 +3,7 @@ var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var bodyParser = require('body-parser');
 var dbCOnnectionObj;
+
 // Connect to the db
 var port = process.env.PORT || 8000
 MongoClient.connect("mongodb://admin:password@ds145359.mlab.com:45359/globe_trot", function(err, db) {
@@ -14,6 +15,7 @@ MongoClient.connect("mongodb://admin:password@ds145359.mlab.com:45359/globe_trot
       console.log('Error in connecting to database');
   }
 });
+
 //adding static files
 app.use('/static', express.static('static'));
 
@@ -38,9 +40,6 @@ app.post('/login', function (req, res) {
     });
 });
 
-
-
-
 app.post('/addtrip', function (req, res) {
     var collection = dbCOnnectionObj.collection('TravelDetails');
     collection.insertOne({
@@ -58,15 +57,25 @@ app.post('/addtrip', function (req, res) {
    });
 });
 
-
-
 app.get('/dashboard', function (req, res) {
     res.sendFile(__dirname + '/dashboard.html');
 });
 
 app.get('/trips', function (req, res) {
-    console.log("response",req.data);
-    res.sendFile(__dirname + '/trips.html');
+    //res.sendFile(__dirname + '/trips.html');
+    var dataTrips = [];
+    var collection = dbCOnnectionObj.collection('trips');
+    collection.find({origin : Rupali},function(err, trips) {
+        trips.each(function(err, item){
+            if(item)
+                dataTrips.push(item);
+        });
+    });
+    setTimeout(function(){
+        res.render('trips', {tripsData: dataTrips});
+        res.end();
+    },500);
+
 });
 
 app.get('/addtrip', function (req, res) {
@@ -81,11 +90,11 @@ app.get('/fire', function (req, res) {
 app.get('/mytrips', function (req, res) {
     var userTrips = [];
     var collection = dbCOnnectionObj.collection('TravelDetails');
-    console.log('collection',collection);
     collection.find({user: 'r@gmail.com'}, function(err, trips) {
         trips.each(function(err, item){
          if(item)
             userTrips.push(item);
+            console.log("userTrips",userTrips);
         });
     });
     setTimeout(function(){
